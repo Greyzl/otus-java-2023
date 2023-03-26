ecc41049ce332fa3526b5491a06e182f96258499
greyzl <sergey_s96@mail.ru>
2023-03-26 18:38:06 +0400

    Set dequeue to private

--- hw02-collections/src/main/java/homework/CustomerReverseOrder.java
+++ hw02-collections/src/main/java/homework/CustomerReverseOrder.java
@@ -8,7 +8,7 @@ public class CustomerReverseOrder {
 
     //todo: 2. надо реализовать методы этого класса
     //надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
-    Deque<Customer> customers = new LinkedList<>();
+    private Deque<Customer> customers = new LinkedList<>();
     public void add(Customer customer) {
         customers.add(customer);
     }
