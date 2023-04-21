5a4f48dc1c0b1506ed09ebc06193280a43507dff
Шилков Сергей Сергеевич <sshilkov@alfabank.ru>
2023-04-21 21:45:07 +0500

    1. Добавил сохранение методов с аннотациями при создании хэндлера чтобы не рефлексировать 2. Заменил проверку имен типов на проверку самих типов в сравнении методов 3. Добавил форматтер для логирования

--- hw05-proxy/src/main/java/ru/otus/aop/logging/LoggingHandler.java
+++ hw05-proxy/src/main/java/ru/otus/aop/logging/LoggingHandler.java
@@ -1,34 +1,53 @@
 package ru.otus.aop.logging;
 
 import ru.otus.annotation.Log;
+import ru.otus.formatter.LogMethodFormatter;
 import ru.otus.service.CalculatorService;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
 
 public class LoggingHandler implements InvocationHandler {
 
     private final CalculatorService calculatorService;
 
+    private final LogMethodFormatter logMethodFormatter = new LogMethodFormatter();
+
+    private final Set<Method> loggingMethods;
+
     public LoggingHandler(CalculatorService calculatorService){
         this.calculatorService = calculatorService;
+        loggingMethods = getLogAnnotatedMethods(calculatorService);
     }
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        Class<?> calculatorServiceClass = calculatorService.getClass();
-        Method[] calculatorServiceMethods = calculatorServiceClass.getDeclaredMethods();
-        for (Method calculatorServiceMethod : calculatorServiceMethods){
-            if (isSameMethods(method, calculatorServiceMethod) &&
-                    calculatorServiceMethod.getDeclaredAnnotation(Log.class) != null){
-                StringBuilder builder = new StringBuilder();
-                for (Object object: args){
-                    builder.append(object).append(" ");
-                }
-                System.out.println("executed method: " + calculatorServiceMethod.getName()
-                        + ", params:" + builder);
-            }
+        if (isLoggingMethod(method)){
+            System.out.println(logMethodFormatter.format(method, args));
         }
         return method.invoke(calculatorService,args);
+    }
+
+    private Set<Method> getLogAnnotatedMethods(Object object){
+        Class<?> objectClass = object.getClass();
+        Method[] objectClassDeclaredMethods = objectClass.getDeclaredMethods();
+        Set<Method> annotatedMethods = new HashSet<>();
+        for (Method objectClassMethod : objectClassDeclaredMethods){
+            if (objectClassMethod.getDeclaredAnnotation(Log.class) != null){
+                annotatedMethods.add(objectClassMethod);
+            }
+        }
+        return annotatedMethods;
+    }
+
+    private Boolean isLoggingMethod(Method method){
+        for (Method loggingMethod : loggingMethods){
+            if (isSameMethods(method, loggingMethod)){
+                return true;
+            }
+        }
+        return false;
     }
 
     private Boolean isSameMethods(Method invokedMethod, Method proxiedObjectMethod){
@@ -40,8 +59,7 @@ public class LoggingHandler implements InvocationHandler {
         Class<?>[] proxyMethodParamTypes = invokedMethod.getParameterTypes();
         Class<?>[] objectTestMethodParamTypes = proxiedObjectMethod.getParameterTypes();
         for (int paramIndex = 0; paramIndex < invokedMethod.getParameterCount(); paramIndex++){
-            if (!objectTestMethodParamTypes[paramIndex].getName().equals(
-                    proxyMethodParamTypes[paramIndex].getName())){
+            if (objectTestMethodParamTypes[paramIndex] != proxyMethodParamTypes[paramIndex]){
                 sameSignature = false;
                 break;
             }
